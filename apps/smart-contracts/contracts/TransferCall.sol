079275cd2a5cf7ec7bc2f7501f99f106847e5c6c
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-02-25 23:34:01 -0700

    add single transfer / encode

--- apps/smart-contracts/contracts/TransferCall.sol
+++ apps/smart-contracts/contracts/TransferCall.sol
@@ -1,9 +1,23 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.18;
 
 error Failed();
 
 contract TransferCall {
+    function singleEncodeTransfer(
+        address _token,
+        address _receiver,
+        bytes4 _functionSig
+    ) external payable {
+        (bool success, ) = _token.call(_functionSig, msg.sender, _receiver, 10);
+        if (!success) revert Failed();
+    }
+
+    function singleTransfer(address _token, bytes _data) external payable {
+        (bool success, ) = _token.call(_data);
+        if (!success) revert Failed();
+    }
+
     function multiTransfer(
         address[] calldata _tokens,
         bytes[] calldata _data
