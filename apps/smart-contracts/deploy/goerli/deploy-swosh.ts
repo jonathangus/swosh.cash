ae9f8b304572bec806eafc8801a7b1d8a9ea9a2c
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-02-28 16:18:57 -0700

    swosh deploy script

---
+++ apps/smart-contracts/deploy/goerli/deploy-swosh.ts
@@ -0,0 +1,45 @@
+import { ethers, network } from 'hardhat';
+import { printDetails, writeDeploymentDetails } from '../utils/utils';
+import SWOSH_ABI from '../../abi/Swosh.json';
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  // Contract Factories
+  const swoshFactory = await ethers.getContractFactory('Swosh', deployer);
+
+  console.log('');
+  console.log(
+    '-------------------------------------------------------- SWOSH DEPLOYMENT ---------------------------------------------------'
+  );
+  console.log('');
+
+  // Deploy Swosh contract
+  const swosh = await swoshFactory.deploy({
+    pollingInterval: 50000,
+    timeout: 0,
+  });
+  await swosh.deployed();
+  let txReceipt = await ethers.provider.send('eth_getTransactionReceipt', [
+    swosh.deployTransaction.hash,
+  ]);
+  writeDeploymentDetails(
+    network.name,
+    'Swosh',
+    swosh.address,
+    [],
+    SWOSH_ABI,
+    +txReceipt.blockNumber
+  );
+
+  // Log deployment details to console
+  printDetails(network.name, 'Swosh', swosh.address);
+
+  console.log('');
+  console.log(
+    '------------------------------------------------------------------------------------------------------------------------------'
+  );
+  console.log('');
+}
+
+main();
