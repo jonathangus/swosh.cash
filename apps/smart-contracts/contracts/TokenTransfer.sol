4e08786dd8e93bc7f9c10ea27a284c5f14da866b
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-02-28 15:03:50 -0700

    draft

--- apps/smart-contracts/contracts/TokenTransfer.sol
+++ apps/smart-contracts/contracts/TokenTransfer.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.18;
 
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import {IERC721} from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+import {IERC1155} from '@openzeppelin/contracts/token/ERC1155/IERC1155.sol';
 
 error INVALID_PARAM();
 
@@ -60,15 +61,4 @@ contract TokenTransfer {
             IERC20(_token).transferFrom(msg.sender, _receivers[i], _amounts[i]);
         }
     }
-
-    // function multiTokenRecipientTransfer(
-    //     IERC20[] calldata _tokens,
-    //     address[] calldata _receiver,
-    //     uint256[] calldata _amounts
-    // ) external {
-    //     if (_tokens.length != _amounts.length) revert INVALID_PARAM();
-    //     for (uint256 i = 0; i < _tokens.length; ++i) {
-    //         _tokens[i].transferFrom(msg.sender, _receiver, _amounts[i]);
-    //     }
-    // }
 }
