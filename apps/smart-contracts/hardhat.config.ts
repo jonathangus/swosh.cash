a6ce4a42b05980d4ac11c0d445d8e87579b9eb9d
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-03-01 17:13:30 -0700

    update hardhat config

--- apps/smart-contracts/hardhat.config.ts
+++ apps/smart-contracts/hardhat.config.ts
@@ -23,9 +23,12 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
 });
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
+const MAINNET_ALCHEMY_KEY = process.env.MAINNET_ALCHEMY_KEY;
+const OPTIMISM_ALCHEMY_KEY = process.env.OPTIMISM_ALCHEMY_KEY;
 const GOERLI_ALCHEMY_KEY = process.env.GOERLI_ALCHEMY_KEY;
-
+const POLYGON_ALCHEMY_KEY = process.env.POLYGON_ALCHEMY_KEY;
+const ARBITRUM_ALCHEMY_KEY = process.env.ARBITRUM_ALCHEMY_KEY;
+  
 const config: HardhatUserConfig = {
   solidity: '0.8.18',
   defaultNetwork: 'hardhat',
@@ -40,27 +43,43 @@ const config: HardhatUserConfig = {
     },
     mainnet: {
       chainId: 1,
-      url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+      url: `https://eth-mainnet.g.alchemy.com/v2/${MAINNET_ALCHEMY_KEY}`,
       accounts: [`${PRIVATE_KEY}`],
-      deploy: ['deploy/mainnet/ethereum'],
     },
     optimism: {
       chainId: 10,
-      url: `https://opt-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+      url: `https://opt-mainnet.g.alchemy.com/v2/${OPTIMISM_ALCHEMY_KEY}`,
       accounts: [`${PRIVATE_KEY}`],
-      deploy: ['deploy/mainnet/optimism'],
+    },
+    arbitrum: {
+      chainId: 10,
+      url: `https://arb-mainnet.g.alchemy.com/v2/${ARBITRUM_ALCHEMY_KEY}`,
+      accounts: [`${PRIVATE_KEY}`],
+    },
+    polygon: {
+      chainId: 137,
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${POLYGON_ALCHEMY_KEY}`,
+      accounts: [`${PRIVATE_KEY}`],
+    },
+    baseGoerli: {
+      chainId: 84531,
+      url: 'https://goerli.base.org',
+      accounts: [`${PRIVATE_KEY}`],
+    },
+    scrollTestnet: {
+      chainId: 534353,
+      url: `https://alpha-rpc.scroll.io/l2`,
+      accounts: [`${PRIVATE_KEY}`],
     },
     goerli: {
       chainId: 5,
       url: `https://eth-goerli.g.alchemy.com/v2/${GOERLI_ALCHEMY_KEY}`,
       accounts: [`${PRIVATE_KEY}`],
-      deploy: ['deploy/goerli'],
     },
     optimismGoerli: {
       chainId: 420,
       url: `https://opt-goerli.g.alchemy.com/v2/${ALCHEMY_KEY}`,
       accounts: [`${PRIVATE_KEY}`],
-      deploy: ['deploy/testnet/optimismGoerli'],
     },
   },
   namedAccounts: {
