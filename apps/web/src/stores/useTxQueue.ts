da05aa0bbb3b2f5772cbaa332ea240de89dd6114
Jonathan <jonathangus@proton.me>
2023-03-02 15:21:15 -0700

    build

--- apps/web/src/stores/useTxQueue.ts
+++ apps/web/src/stores/useTxQueue.ts
@@ -3,17 +3,8 @@ import { create } from 'zustand';
 
 interface SelectionState {
   queue: Token[];
-  setQueue: (token: Token) => void;
 }
 
 export const useTxQueue = create<SelectionState>((set) => ({
   queue: [],
-  setSelected: (token: Token) =>
-    set((state) => ({
-      selected: [...state.selected, token],
-    })),
-  removeSelected: (token: Token) =>
-    set((state) => ({
-      selected: state.selected.filter((item) => item.id !== token.id),
-    })),
 }));
